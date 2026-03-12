073e747d45d757ac1d005e50db5ffdbcbbc96f79
alexmkinn-coder <alex.m.kinn@gmail.com>
2026-03-12 20:09:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-// Get elements
+// Get modal elements
 const modal = document.getElementById("pdfModal");
 const btn = document.getElementById("viewBtn");
-const span = document.getElementsByClassName("close")[0];
+const closeSpan = document.querySelector(".close");
 
-// Open the modal when button is clicked
+// Open modal on button click
 btn.onclick = function() {
     modal.style.display = "block";
+    document.body.style.overflow = "hidden"; // Stop background scrolling
 }
 
-// Close the modal when 'x' is clicked
-span.onclick = function() {
+// Close modal on 'x' click
+closeSpan.onclick = function() {
     modal.style.display = "none";
+    document.body.style.overflow = "auto"; // Resume background scrolling
 }
 
-// Close the modal if user clicks outside of the window
+// Close modal if user clicks outside the image
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        document.body.style.overflow = "auto";
     }
 }
